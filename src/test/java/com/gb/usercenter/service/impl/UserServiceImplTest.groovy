9ccf02ed853ce509bd8c2c3f6db0ce3e889e60fa
gengbin <bin.geng@seres.cn>
2023-05-23 18:05:57 +0800

    add test

--- src/test/java/com/gb/usercenter/service/impl/UserServiceImplTest.groovy
+++ src/test/java/com/gb/usercenter/service/impl/UserServiceImplTest.groovy
@@ -12,46 +12,45 @@ class UserServiceImplTest extends Specification {
 
     def "Register - success"() {
         given: "init param"
-            def userMapper = Mock(UserMapper)
-            def userService = Mock(UserService)
-            def userName = "张三"
-            def phone = "18847160594"
-            def password = "123456"
-            def checkPassword = "123456"
+        def userMapper = Mock(UserMapper)
+        def userService = Mock(UserService)
+        def userName = "张三"
+        def phone = "18847160594"
+        def password = "123456"
+        def checkPassword = "123456"
+
         when: "call register method"
-            def id = userService.register(userName,phone,password,checkPassword)
+        def id = userService.register(userName, phone, password, checkPassword)
+
         then: "register success"
-            userMapper.selectCount(_ as User) >> 0
-            userMapper.insertSelective(_ as User) >> {
-                User user ->
-                    user.id = 1
-                    user
-            }
+        userMapper.selectCount(_ as User) >> 0
+        userMapper.insertSelective(_ as User) >> {
+            User user ->
+                user.id = 1
+                user
+        }
     }
-
-
-
 
 
     def "Login 异常情况 "() {
         given:
-            def userService = new UserServiceImpl()
-            def request = Mock(HttpServletRequest)
+        def userService = new UserServiceImpl()
+        def request = Mock(HttpServletRequest)
         when: "调用 login 方法"
-            userService.login(phone, password, request)
+        userService.login(phone, password, request)
 
         then: "捕获异常"
-            def exception = thrown(expectedException)
-                exception.message == expectedMessage
+        def exception = thrown(expectedException)
+        exception.message == expectedMessage
 
         where: "校验参数"
-            phone           ||  password ||  expectedException   ||  expectedMessage
-            ""              ||      ""   ||    CustomException   ||  "请输入手机号和密码"
-            ""              ||    "test" ||   CustomException    ||  "请输入手机号和密码"
-            "18847160591"   ||      ""   ||   CustomException    ||  "请输入手机号和密码"
-            "18847160591"   ||      "test"   ||   CustomException    ||  "密码不能少于6位"
-            "11543536354"   ||    "test" ||    CustomException   ||     "手机号格式错误"
-            "13543-36354"   ||    "test" ||    CustomException   ||     "手机号格式错误"
+        phone         | password || expectedException || expectedMessage
+        ""            | ""       || CustomException   || "请输入手机号和密码"
+        ""            | "test"   || CustomException   || "请输入手机号和密码"
+        "18847160591" | ""       || CustomException   || "请输入手机号和密码"
+        "18847160591" | "test"   || CustomException   || "密码不能少于6位"
+        "11543536354" | "test"   || CustomException   || "手机号格式错误"
+        "13543-36354" | "test"   || CustomException   || "手机号格式错误"
     }
 
     def "SearchUsers"() {
